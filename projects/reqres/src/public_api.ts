a4da5c5014f7404caf0810a35576206346245c47
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-07-28 20:24:00 -0500

    Making our new models available for use
    In the projects/reqres/src/public_api.ts file, add the following:
    export * from './lib/model/user';
    export * from './lib/model/users';

--- projects/reqres/src/public_api.ts
+++ projects/reqres/src/public_api.ts
@@ -5,3 +5,5 @@
 export * from './lib/reqres.service';
 export * from './lib/reqres.component';
 export * from './lib/reqres.module';
+export * from './lib/model/user';
+export * from './lib/model/users';
