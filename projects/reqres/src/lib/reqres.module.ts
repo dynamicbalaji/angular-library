8ce137f677e416150eac2b136482015a33fd0d05
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-07-28 20:32:51 -0500

    Generating new component in reqres project
    ng g component users --project=reqres

--- projects/reqres/src/lib/reqres.module.ts
+++ projects/reqres/src/lib/reqres.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ReqresComponent } from './reqres.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [ReqresComponent],
+  declarations: [ReqresComponent, UsersComponent],
   exports: [ReqresComponent]
 })
 export class ReqresModule { }
