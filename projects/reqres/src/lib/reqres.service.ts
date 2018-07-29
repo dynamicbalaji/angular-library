565736db63641bbde7fefba48f38934885acbbb8
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-07-28 20:29:40 -0500

    Adding service code changes
    This service is pretty straightforward, just a simple ‘get’ to the API. One thing to note is the property providedIn: 'root'. This is new to Angular 6 and it specifies what module the service is to be set up for, in this case, root. This allows for tree shaking to happen for services now.

--- projects/reqres/src/lib/reqres.service.ts
+++ projects/reqres/src/lib/reqres.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Users } from './model/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReqresService {
 
-  constructor() { }
+  private readonly apiRoot = 'https://reqres.in/api';
+ 
+  constructor(private httpClient: HttpClient) { }
+ 
+  getUsers(page: number): Observable<Users> {
+    const params = new HttpParams().set('page', String(page));
+    //return this.httpClient.get<Users>(this.apiRoot+'/users', {params: params});
+    return this.httpClient.get<Users>(`${this.apiRoot}/users`, {params: params});
+  }
 }
