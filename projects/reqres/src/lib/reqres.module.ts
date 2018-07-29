3e63b67c588c9b19be64c0b1c85651f4873782f8
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-07-28 20:39:00 -0500

    Importing CommonModule & HttpClientModule in reqres project module

--- projects/reqres/src/lib/reqres.module.ts
+++ projects/reqres/src/lib/reqres.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ReqresComponent } from './reqres.component';
 import { UsersComponent } from './users/users.component';
 
 @NgModule({
   imports: [
+    CommonModule, HttpClientModule
   ],
   declarations: [ReqresComponent, UsersComponent],
   exports: [ReqresComponent]
